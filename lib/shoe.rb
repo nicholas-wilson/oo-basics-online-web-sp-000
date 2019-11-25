d6cafe2863ddd21cfadf3131ac5596929f63b364
Nicholas Wilson <nicholas.wilson@nevada.unr.edu>
2019-11-25 19:45:24 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -2,7 +2,7 @@
 class Shoe
   attr_reader :brand
   attr_accessor :color, :size, :material, :condition
-  
+
   def initialize(brand)
     @brand = brand
   end
@@ -11,4 +11,4 @@ class Shoe
     puts "The shoe has been repaired."
   end
 
-end  
+end
