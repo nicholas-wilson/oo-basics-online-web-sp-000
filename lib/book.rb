255eca04bcf3919ec8158c1500c5bfe6ad0105ec
Nicholas Wilson <nicholas.wilson@nevada.unr.edu>
2019-11-25 19:31:41 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -2,11 +2,8 @@ class Book
 
   attr_accessor :title, :author_name, :page_count, :genre
 
-  def initialize(title = "And Then There Were None", author_name, page_count, genre)
+  def initialize(title = "And Then There Were None")
     @title = title
-    @author_name = author_name
-    @page_count = page_count
-    @genre = genre
   end
 
   def turn_page(page_count)
